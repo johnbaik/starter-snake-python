494733ba403dbbbd06f6b2d56f980311cb44a981
Ioannis Baikousis <john.baik@yahoo.gr>
2024-11-22 16:48:59 +0200

    game start

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def info() -> typing.Dict:
 
 # start is called when your Battlesnake begins a game
 def start(game_state: typing.Dict):
-    print("GAME START")
+    print("GAME START1")
 
 
 # end is called when your Battlesnake finishes a game
